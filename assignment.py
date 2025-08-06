bbc0bd4c8a9240d09e89bec277fd6299e2e99f07
Emaan Waqar <emaanwaqar0102@gmail.com>
2025-08-06 15:39:10 +0500

    Assignment

---
+++ assignment.py
@@ -0,0 +1,7 @@
+def circumference(radius):
+    pi= 3.14159
+    c= pi* radius* 2
+    return c
+radius= float(input("Enter the radius: "))
+print("Circumference of the circle is: ", circumference(radius))
+circumference(radius)
